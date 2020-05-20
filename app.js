261149a45de5379826a195700cbafe2b33a3cf0d
Ardit Daku <arditdaku1@gmail.com>
2020-05-20 10:54:48 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('package');
+const path = require('path');
+const app = express();
+
+
+
+const port = 5000;
+
+
+
